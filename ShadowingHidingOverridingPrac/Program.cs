fc5c5205d542e763f8dd62016c4cb6d624c7205b
Juwan Lee <ljw05w5@gmail.com>
2023-06-01 10:02:56 +0900

    Shadowing 이해하기

--- ShadowingHidingOverridingPrac/Program.cs
+++ ShadowingHidingOverridingPrac/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace ShadowingHidingOverridingPrac
 {
-    internal class Program
+    public class Program
     {
+        public static int num = 10; //클래스 변수
+        public int num2 = 2; //인스턴스 변수 
         static void Main(string[] args)
         {
+            int num = 20;
+            int num2 = 4;
+            
+            Console.WriteLine(num); //지역변수 접근
+            Console.WriteLine(Program.num); //클래스 변수 접근
+            Console.WriteLine((new Program()).num2); //인스턴스 변수 접근
         }
     }
 }
