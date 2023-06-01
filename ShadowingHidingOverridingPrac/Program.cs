762517d2ad5e32b217bfc902579418d5fed72f09
Juwan Lee <ljw05w5@gmail.com>
2023-06-01 10:14:02 +0900

    Shadowing 이해하기

--- ShadowingHidingOverridingPrac/Program.cs
+++ ShadowingHidingOverridingPrac/Program.cs
@@ -8,6 +8,15 @@ namespace ShadowingHidingOverridingPrac
 {
     public class Program
     {
+        class Parent
+        {
+        }
+
+        class Child : Parent
+        {
+
+        }
+
         public static int num = 10; //클래스 변수
         public int num2 = 2; //인스턴스 변수 
         static void Main(string[] args)
@@ -18,6 +27,13 @@ namespace ShadowingHidingOverridingPrac
             Console.WriteLine(num); //지역변수 접근
             Console.WriteLine(Program.num); //클래스 변수 접근
             Console.WriteLine((new Program()).num2); //인스턴스 변수 접근
+        }
+
+        void doSomething()
+        {
+            int number2 = 3;
+            Console.WriteLine(number2);
+            Console.WriteLine(this.number2); // 인스턴스 변수인 경우는 이렇게.. 
         }
     }
 }
